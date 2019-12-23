1ca80e822206bde116801281cc0e5465ed35b750
yixiangyang <15138778469@163.com>
2019-12-23 18:02:28 +0800

    aa

--- src/main/java/com/yixiangyang/java/util/LocalDateUtil.java
+++ src/main/java/com/yixiangyang/java/util/LocalDateUtil.java
@@ -1,17 +1,35 @@
 package com.yixiangyang.java.util;
 
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class LocalDateUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LocalDateUtil.class);
+	private static final String YYYYMMDD = "yyyy-MM-dd";
+	private static final String YYYYMMDDHHMMSS = "yyyy-MM-dd HH:mm:ss";
+	
+	/**
+	 * 获取当前时间
+	 * @return
+	 */
+	public static Date getCurrentDate() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		ZoneId zone = ZoneId.of("Asia/Shanghai");
+		Instant instant = localDateTime.atZone(zone).toInstant();
+		java.util.Date date = Date.from(instant);
+		return date;
+	}
+	
 	/**
 	 * 获取当前时间的毫秒
 	 * @return
@@ -32,14 +50,23 @@ public class LocalDateUtil {
         return date.toInstant().atOffset(ZoneOffset.of("+8")).toLocalDateTime();
     }
  
+    //将java.util.Date 转换为java8 的java.time.LocalDate,默认时区为东8区
+    public static LocalDate dateConvertToLocalDate(Date date) {
+        return date.toInstant().atOffset(ZoneOffset.of("+8")).toLocalDate();
+    }
    
     //将java8 的 java.time.LocalDateTime 转换为 java.util.Date，默认时区为东8区
     public static Date localDateTimeConvertToDate(LocalDateTime localDateTime) {
         return Date.from(localDateTime.toInstant(ZoneOffset.of("+8")));
     }
 	
+  //将java8 的 java.time.LocalDateTime 转换为 java.util.Date，默认时区为东8区
+    public static Date localDateConvertToDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay(ZoneId.of("Asia/Shanghai")).toInstant());
+    }
+    
     /**
-	 * 解析时间字符串成YYYYMMDDHHMMSS
+	 * 解析时间字符串成 yyyy-MM-dd HH:mm:ss
 	 * @param dateText
 	 * @return
 	 */
@@ -50,69 +77,100 @@ public class LocalDateUtil {
 			LocalDateTime dateTime = LocalDateTime.parse(dateText, df);
 			return localDateTimeConvertToDate(dateTime);
 		} catch (Exception e) {
-			
+			LOGGER.error(e.getMessage());
 			return null;
 		}
 	}
 	
+	/**
+	 * 解析时间字符串成 yyyy-MM-dd HH:mm:ss
+	 * @param dateText
+	 * @return
+	 */
+	public static final Date parseYYYYMMDD(String dateText) {
+		if(dateText == null) {return null;}
+		try {
+			DateTimeFormatter df = DateTimeFormatter.ofPattern(YYYYMMDD);
+			LocalDate dateTime = LocalDate.parse(dateText, df);
+			return localDateConvertToDate(dateTime);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			return null;
+		}
+	}
+	
+	/**
+	 * 将毫秒值 转换为日期
+	 * @param time
+	 * @return
+	 */
 	public static final String formatYYYY_MM_DDHHMMSS(long time) {
-		DateTimeFormatter df= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		DateTimeFormatter df= DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS);
 		String longToDateTime = df.format(LocalDateTime.ofInstant(
 		   Instant.ofEpochMilli(time),ZoneId.of("Asia/Shanghai")));
 		return longToDateTime;
 	}
 	
 	/**
-	 * 解析时间字符串成YYYYMMDDHHMMSS
-	 * @param dateText
+	 * 将一个日期加几天 或者减几天
+	 * @param date日期
+	 * @param days 添加的数 
+	 * @return  年-月-日
+	 */
+	public static final String addDays(Date date,Long days) {
+		if(date ==null) {
+			date = new Date();
+		}
+		DateTimeFormatter df= DateTimeFormatter.ofPattern(YYYYMMDD);
+		return df.format(dateConvertToLocalDateTime(date).plusDays(days));
+	}
+	
+	/**
+	 * 获取两个时间年的差值
+	 * @param beforDate
+	 * @param afterDate
 	 * @return
 	 */
-//	public static final Timestamp parseYYYYMMDD(String dateText) {
-//		if(dateText == null) {return null;}
-//		try {
-//			Date date = YYYYMMDDFORMATER.parse(dateText);
-//			return new Timestamp(date.getTime());
-//		} catch (ParseException e) {
-//			LOGGER.error(DateUtil.class+"-parseYYYYMMDD -"+e);
-//			return null;
-//		}
-//	}
-//	
-//	/**
-//	 * 解析时间字符串成YYYYMM
-//	 * @param dateText
-//	 * @return
-//	 */
-//	public static final Timestamp parseYYYYMM(String dateText) {
-//		if(dateText == null) {return null;}
-//		try {
-//			Date date = YYYYMMFORMATER.parse(dateText);
-//			return new Timestamp(date.getTime());
-//		} catch (ParseException e) {
-//			LOGGER.error(DateUtil.class+"-parseYYYYMM -"+e);
-//			return null;
-//		}
-//	}
-//	
-//	/**
-//	 * 比较是否同一天
-//	 * @param date1
-//	 * @param date2
-//	 * @return
-//	 */
-//	public static boolean isSameDay(Date date1, Date date2) {
-//		String day1 = YYYYMMDDFORMATER.format(date1);
-//		String day2 = YYYYMMDDFORMATER.format(date2);
-//		return day1.equals(day2);
-//	}
+	public static final int differYear(Date beforDate,Date afterDate) {
+		LocalDate localBeforDate = dateConvertToLocalDate(beforDate);
+		LocalDate localAfterDate = dateConvertToLocalDate(afterDate);
+		Period period = Period.between(localBeforDate, localAfterDate);
+		return period.getYears();
+	}
 	
+	/**
+	 * 获取两个时间月的差值
+	 * @param beforDate
+	 * @param afterDate
+	 * @return
+	 */
+	public static final int differMonth(Date beforDate,Date afterDate) {
+		LocalDate localBeforDate = dateConvertToLocalDate(beforDate);
+		LocalDate localAfterDate = dateConvertToLocalDate(afterDate);
+		Period period = Period.between(localBeforDate, localAfterDate);
+		return period.getMonths();
+	}
 	
+	/**
+	 * 获取两个时间天的差值
+	 * @param beforDate
+	 * @param afterDate
+	 * @return
+	 */
+	public static final int differDays(Date beforDate,Date afterDate) {
+		LocalDate localBeforDate = dateConvertToLocalDate(beforDate);
+		LocalDate localAfterDate = dateConvertToLocalDate(afterDate);
+		Period period = Period.between(localBeforDate, localAfterDate);
+		return period.getDays();
+	}
     
 	public static void main(String[] args) {
-		System.out.println(getCurrentMillisecond());
-		System.out.println("秒:"+getCurrentSecond());
-		System.out.println(parseYYYYMMDDHHMMSS("2019-12-12 10:47:47"));
-		System.out.println(formatYYYY_MM_DDHHMMSS(parseYYYYMMDDHHMMSS("2019-12-12 10:47:47").getTime()));
-		System.out.println(DateFormat.getInstance());
+//		System.out.println(getCurrentMillisecond());
+//		System.out.println("秒:"+getCurrentSecond());
+//		System.out.println(parseYYYYMMDDHHMMSS("2019-12-12 10:47:47"));
+//		System.out.println(formatYYYY_MM_DDHHMMSS(parseYYYYMMDDHHMMSS("2019-12-12 10:47:47").getTime()));
+//		System.out.println(DateFormat.getInstance());
+//		System.out.println(addDays(new Date(), -2l));
+		System.out.println(differDays(new Date(), parseYYYYMMDD("2019-12-24")));
 	}
 }
